e3b387a7e831a67e0640adb87af13f458e3498aa
Capitán <capitanes@helloworld-school.com>
2022-02-22 10:29:47 +0100

    Ejemplo completo

--- src/main/java/com/helloworld/demomvc/DemomvcApplication.java
+++ src/main/java/com/helloworld/demomvc/DemomvcApplication.java
@@ -7,23 +7,15 @@ import com.helloworld.demomvc.services.implementation.GreetingServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@ImportResource("classpath:beans.xml")
 public class DemomvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemomvcApplication.class, args);
-	}
-
-	@Bean
-	public GreetingService getGreetingService(Environment environment){
-		String greetingServiceType = environment.getProperty("greeting-service-type");
-		if(greetingServiceType.equals("regular")){
-			return new GreetingServiceImpl();
-		} else {
-			return new GreetingDayPartServiceImpl();
-		}
 	}
 
 }
